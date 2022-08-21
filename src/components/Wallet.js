f26fba5e8418ca506fcfbbdce0e4b3f784043653
abhilash yadav <abhilash3rde@gmail.com>
2022-08-21 12:45:36 +0530

    wallet passbok

--- src/components/Wallet.js
+++ src/components/Wallet.js
@@ -7,6 +7,8 @@ import { HiOutlineSpeakerphone } from 'react-icons/hi'
 import { FiChevronDown } from 'react-icons/fi'
 import { RiTimerLine } from 'react-icons/ri'
 
+import '../styles/wallet.css'
+
 const Wallet = () => {
    const [menuCollapse, setMenuCollapse] = useState(true)
 
@@ -20,27 +22,11 @@ const Wallet = () => {
          <Header menuIconClick={menuIconClick} />
          <Sidebar menuCollapse={menuCollapse} />
          <div className="main-content">
-            <div style={{ textAlign: 'center', marginTop: '30px' }}>
-               <h1
-                  style={{
-                     fontSize: '60px',
-                     fontWeight: '600',
-                     color: '#9B51E0'
-                  }}
-               >
-                  ₹35
-               </h1>
-               <p
-                  style={{
-                     color: '#962E40',
-                     fontSize: '24px',
-                     fontWeight: '600'
-                  }}
-               >
-                  Your StreamPala Account Balance{' '}
-               </p>
+            <div className="wallet-balance">
+               <h1>₹35</h1>
+               <p>Your StreamPala Account Balance </p>
             </div>
-            <div style={{ padding: '20px' }}>
+            <div className="your-earnings-summary">
                <div className="row gx-5 gy-4">
                   <div className="col-lg-6">
                      <div>
@@ -52,185 +38,83 @@ const Wallet = () => {
                            </div>
                         </div>
                      </div>
-                     <div
-                        className="row gy-2 gx-4"
-                        style={{ marginTop: '20px' }}
-                     >
-                        <div className="col-lg-6">
-                           <div
-                              style={{
-                                 backgroundColor: '#fff',
-                                 borderRadius: '35px',
-                                 padding: '20px'
-                              }}
-                           >
-                              <div
+                  </div>
+                  <div className="col-lg-6">
+                     <div style={{ borderBottom: '1px solid #9B51E0' }}>
+                        <div
+                           style={{
+                              fontSize: '20px',
+                              padding: '15px 30px',
+                              display: 'flex',
+                              color: '#9B51E0',
+                              justifyContent: 'space-between'
+                           }}
+                        >
+                           <div style={{ display: 'flex' }}>
+                              <RiTimerLine
                                  style={{
-                                    textAlign: 'center'
+                                    marginTop: '5px',
+                                    marginRight: '10px',
+                                    fontSize: '25px'
                                  }}
-                              >
-                                 <p
-                                    style={{
-                                       margin: '0px',
-                                       fontSize: '30px',
-                                       color: '#962E40',
-                                       fontWeight: 'bold'
-                                    }}
-                                 >
-                                    ####
-                                 </p>
-                                 <p
-                                    style={{
-                                       margin: '0px',
-                                       fontSize: '18px',
-                                       color: '#6F6F6F'
-                                    }}
-                                 >
-                                    Views
-                                 </p>
-                              </div>
+                              />
+                              <p style={{ margin: '0px' }}>All Time</p>
                            </div>
-                        </div>
-                        <div className="col-lg-6">
-                           <div
+                           <FiChevronDown
                               style={{
-                                 backgroundColor: '#fff',
-                                 borderRadius: '35px',
-                                 padding: '20px'
+                                 fontSize: '25px',
+                                 marginTop: '5px'
                               }}
-                           >
-                              <div
-                                 style={{
-                                    textAlign: 'center'
-                                 }}
-                              >
-                                 <p
-                                    style={{
-                                       margin: '0px',
-                                       fontSize: '30px',
-                                       color: '#962E40',
-                                       fontWeight: 'bold'
-                                    }}
-                                 >
-                                    ####
-                                 </p>
-                                 <p
-                                    style={{
-                                       margin: '0px',
-                                       fontSize: '18px',
-                                       color: '#6F6F6F'
-                                    }}
-                                 >
-                                    Streams
-                                 </p>
-                              </div>
-                           </div>
+                           />
                         </div>
                      </div>
                   </div>
-                  <div className="col-lg-6">
-                     <div>
-                        <div style={{ borderBottom: '1px solid #9B51E0' }}>
+                  <div className="row gy-4">
+                     <div className="col-lg-3">
+                        <div className="earnings-summary-card">
                            <div
                               style={{
-                                 fontSize: '20px',
-                                 padding: '15px 30px',
-                                 display: 'flex',
-                                 color: '#9B51E0',
-                                 justifyContent: 'space-between'
+                                 textAlign: 'center'
                               }}
                            >
-                              <div style={{ display: 'flex' }}>
-                                 <RiTimerLine
-                                    style={{
-                                       marginTop: '5px',
-                                       marginRight: '10px',
-                                       fontSize: '25px'
-                                    }}
-                                 />
-                                 <p style={{ margin: '0px' }}>All Time</p>
-                              </div>
-                              <FiChevronDown
-                                 style={{
-                                    fontSize: '25px',
-                                    marginTop: '5px'
-                                 }}
-                              />
+                              <p>####</p>
+                              <p>Views</p>
                            </div>
                         </div>
                      </div>
-                     <div
-                        className="row gy-2 gx-4"
-                        style={{ marginTop: '20px' }}
-                     >
-                        <div className="col-lg-6">
+                     <div className="col-lg-3">
+                        <div className="earnings-summary-card">
                            <div
                               style={{
-                                 backgroundColor: '#fff',
-                                 borderRadius: '35px',
-                                 padding: '20px'
+                                 textAlign: 'center'
                               }}
                            >
-                              <div
-                                 style={{
-                                    textAlign: 'center'
-                                 }}
-                              >
-                                 <p
-                                    style={{
-                                       margin: '0px',
-                                       fontSize: '30px',
-                                       color: '#962E40',
-                                       fontWeight: 'bold'
-                                    }}
-                                 >
-                                    ####
-                                 </p>
-                                 <p
-                                    style={{
-                                       margin: '0px',
-                                       fontSize: '18px',
-                                       color: '#6F6F6F'
-                                    }}
-                                 >
-                                    Clicks
-                                 </p>
-                              </div>
+                              <p>####</p>
+                              <p>Streams</p>
                            </div>
                         </div>
-                        <div className="col-lg-6">
+                     </div>
+                     <div className="col-lg-3">
+                        <div className="earnings-summary-card">
                            <div
                               style={{
-                                 backgroundColor: '#fff',
-                                 borderRadius: '35px',
-                                 padding: '20px'
+                                 textAlign: 'center'
                               }}
                            >
-                              <div
-                                 style={{
-                                    textAlign: 'center'
-                                 }}
-                              >
-                                 <p
-                                    style={{
-                                       margin: '0px',
-                                       fontSize: '30px',
-                                       color: '#962E40',
-                                       fontWeight: 'bold'
-                                    }}
-                                 >
-                                    ####
-                                 </p>
-                                 <p
-                                    style={{
-                                       margin: '0px',
-                                       fontSize: '18px',
-                                       color: '#6F6F6F'
-                                    }}
-                                 >
-                                    Spends
-                                 </p>
-                              </div>
+                              <p>####</p>
+                              <p>Clicks</p>
+                           </div>
+                        </div>
+                     </div>
+                     <div className="col-lg-3">
+                        <div className="earnings-summary-card">
+                           <div
+                              style={{
+                                 textAlign: 'center'
+                              }}
+                           >
+                              <p>####</p>
+                              <p>Spends</p>
                            </div>
                         </div>
                      </div>
@@ -239,10 +123,39 @@ const Wallet = () => {
                <div style={{ textAlign: 'center', padding: '40px ' }}>
                   <a href="#">Analyze your performance >> </a>
                </div>
-               <div>
+               <div className="passbook-container">
                   <h5 style={{ fontWeight: '700' }}>
                      Your StramPala Wallet Passbook
                   </h5>
+                  <div className="passbook-card">
+                     <div>
+                        <div style={{ display: 'flex' }}>
+                           <img
+                              src={require('../img/profile-bg.png')}
+                              alt=""
+                              className="passbook-pic"
+                              style={{}}
+                           />
+                           <div>
+                              {' '}
+                              <h6
+                                 style={{ fontSize: '16px', fontWeight: '700' }}
+                              >
+                                 Campaign name in long format
+                              </h6>
+                              <span>04.09.2021</span>
+                           </div>
+                        </div>
+                     </div>
+                     <div>
+                        <h1 style={{ fontSize: '36px', fontWeight: '700' }}>
+                           +₹20
+                        </h1>
+                        <p style={{ fontSize: '16px', marginBottom: '0px' }}>
+                           Closing balance: ₹15
+                        </p>
+                     </div>
+                  </div>
                </div>
             </div>
          </div>
